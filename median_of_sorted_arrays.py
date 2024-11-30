fb135043feee965a2ab668fdaa015a2e0fc0e25b
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2024-11-30 10:08:41 -0800

    Create median_of_sorted_arrays.py

---
+++ median_of_sorted_arrays.py
@@ -0,0 +1,9 @@
+  def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+      import statistics
+      if len(nums1) > 0:
+          list1_median = statistics.median(nums1)
+      if len(nums2) > 0:
+          for item in nums2:
+              nums1.append(item)
+      list2_median = statistics.median(nums1)
+      return list2_median
