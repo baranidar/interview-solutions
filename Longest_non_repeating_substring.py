9b49ae465a7514ec11ca3a9d8e82d79a42f19202
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2024-11-30 10:08:09 -0800

    Update Longest_non_repeating_substring.py

--- Longest_non_repeating_substring.py
+++ Longest_non_repeating_substring.py
@@ -1,26 +1,25 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        substring_counter = 0
-        text = list(s)
-        substring = ""
-        longest_substring_length = 0
-        outer_string_counter = 0
-        for letter in text:
-            string_counter = outer_string_counter
-            #print(letter)
-            while string_counter <= len(text)-1:
+def lengthOfLongestSubstring(self, s: str) -> int:
+    substring_counter = 0
+    text = list(s)
+    substring = ""
+    longest_substring_length = 0
+    outer_string_counter = 0
+    for letter in text:
+        string_counter = outer_string_counter
+        #print(letter)
+        while string_counter <= len(text)-1:
+            #print(string_counter, substring, substring_counter)
+            if s[string_counter] not in substring:
+                substring += s[string_counter]
+                substring_counter += 1
+                if substring_counter > longest_substring_length:
+                    longest_substring_length =  substring_counter  
+                string_counter += 1
+            else:
+                #print("Character is repeating")
+                substring_counter = 0
+                substring = ""
+                string_counter = len(text) + 1
                 #print(string_counter, substring, substring_counter)
-                if s[string_counter] not in substring:
-                    substring += s[string_counter]
-                    substring_counter += 1
-                    if substring_counter > longest_substring_length:
-                        longest_substring_length =  substring_counter  
-                    string_counter += 1
-                else:
-                    #print("Character is repeating")
-                    substring_counter = 0
-                    substring = ""
-                    string_counter = len(text) + 1
-                    #print(string_counter, substring, substring_counter)
-            outer_string_counter += 1
-        return longest_substring_length
+        outer_string_counter += 1
+    return longest_substring_length
